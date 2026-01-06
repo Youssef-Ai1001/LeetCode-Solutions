274606b3900b9cfdca57dbc326cc6b4c59c1c722
Youssef-Ai1001 <ytaha8586@gmail.com>
2026-01-06 13:27:10 +0200

    Add main execution block and test cases for "Find All Numbers Disappeared in an Array" solution

--- 448. Find All Numbers Disappeared in an Array.py
+++ 448. Find All Numbers Disappeared in an Array.py
@@ -7,5 +7,17 @@ class Solution:
         return list(set(range(1, len(nums) + 1)) - set(nums))
 
 
-test = Solution()
-print(test.findDisappearedNumbers(nums=[4, 3, 2, 7, 8, 2, 3, 1]))
+if __name__ == "__main__":
+    s = Solution()
+
+    tests = [
+        [4, 3, 2, 7, 8, 2, 3, 1],
+        [1, 1],
+        []
+    ]
+
+    for t in tests:
+        nums = t.copy()
+        print("Input:", nums)
+        print("Result:", s.findDisappearedNumbers(nums=nums))
+        print('-' * 30)
