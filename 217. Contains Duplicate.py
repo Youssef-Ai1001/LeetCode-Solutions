55c426589ef6375774c85f3e26f5efbe6a2cff2b
Youssef-Ai1001 <ytaha8586@gmail.com>
2026-01-06 12:44:40 +0200

    Implement solution for "Contains Duplicate" problem

---
+++ 217. Contains Duplicate.py
@@ -0,0 +1,17 @@
+from typing import List
+
+
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+
+        set_nums = set()
+
+        for i in nums:
+
+            if i not in set_nums:
+                set_nums.add(i)
+
+            else:
+                return True
+
+        return False
