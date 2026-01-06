2290faa58fd94f3b04d9ae647cf15046df14f624
Youssef-Ai1001 <ytaha8586@gmail.com>
2026-01-06 13:25:21 +0200

    Add main execution block and test cases for "Single Number" solution

--- 136. Single Number.py
+++ 136. Single Number.py
@@ -8,3 +8,18 @@ class Solution:
         for num in nums:
             result ^= num
         return result
+
+
+if __name__ == "__main__":
+    s = Solution()
+
+    tests = [
+        [2, 2, 1],
+        [4, 1, 2, 1, 2],
+    ]
+
+    for t in tests:
+        nums = t.copy()
+        print("Input:", nums)
+        print("Result:", s.singleNumber(nums))
+        print("-" * 30)
