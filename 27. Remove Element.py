7f977b575c0670c3d5eb09cb9595ffb3bf680213
Youssef-Ai1001 <ytaha8586@gmail.com>
2026-01-08 13:13:31 +0200

    Add implementation and test cases for "Remove Element" solution

---
+++ 27. Remove Element.py
@@ -0,0 +1,30 @@
+from typing import List
+
+
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+
+        k = 0
+        
+        for i in range(len(nums)):
+            if nums[i] != val:
+                nums[k] = nums[i]
+                k+=1
+                
+        return k
+
+
+if __name__ == "__main__":
+    solution = Solution()
+    
+    # Test case 1
+    nums1 = [3, 2, 2, 3]
+    val1 = 3
+    k1 = solution.removeElement(nums1, val1)
+    print(f"Test 1: k = {k1}, nums = {nums1[:k1]}")
+    
+    # Test case 2
+    nums2 = [0, 1, 2, 2, 3, 0, 4, 2]
+    val2 = 2
+    k2 = solution.removeElement(nums2, val2)
+    print(f"Test 2: k = {k2}, nums = {nums2[:k2]}")
